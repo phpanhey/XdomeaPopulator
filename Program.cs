64eac4789149dc64f55c72a1cfe9a864316db023
Philipp Panhey <philipp.panhey@dataport.de>
2025-11-27 10:54:12 +0100

    feat: Update XML template and output handling; add README instructions

--- Program.cs
+++ Program.cs
@@ -1,11 +1,133 @@
 ﻿using Populator;
 
-string template = @"
-<Config>
-    <NAME>${NAME}</NAME>
-    <IP_ADDRESS>${IP_ADDRESS}</IP_ADDRESS>
-    <Attachments>${ATTACHMENTS}<Attachments>
-</Config>";
+string template = """
+<ns2:FVDaten.SGOAblegen.0605 xmlns:ns2="urn:xoev-de:xdomea:schema:3.0.0"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="urn:xoev-de:xdomea:xdomea-0605.xsd">
+    <ns2:Kopf>
+        <ns2:ProzessID>e6c47537-0594-4d8c-be53-89b5e84a85f6</ns2:ProzessID>
+        <ns2:Nachrichtentyp>
+            <code>0605</code>
+        </ns2:Nachrichtentyp>
+        <ns2:Erstellungszeitpunkt>2025-07-22T11:41:05.9544231+02:00</ns2:Erstellungszeitpunkt>
+        <ns2:Absender />
+        <ns2:Empfaenger>
+            <ns2:Taetigkeit>Dinge von A nach B schicken</ns2:Taetigkeit>
+        </ns2:Empfaenger>
+        <ns2:Empfangsbestaetigung>true</ns2:Empfangsbestaetigung>
+        <ns2:Importbestaetigung>true</ns2:Importbestaetigung>
+        <ns2:Erstellerkennung>VIC-API</ns2:Erstellerkennung>
+        <ns2:ErstellerRolle>VIC-API</ns2:ErstellerRolle>
+        <ns2:Stapel>true</ns2:Stapel>
+        <ns2:Stapellaenge>2</ns2:Stapellaenge>
+    </ns2:Kopf>
+    <ns2:SchriftgutobjektZumAblegen>
+        <!-- abwaertskompatibel mit primaerdokument machen -->
+        <ns2:FachverfahrenObjektID>10000000-0000-0000-0000-000000000000_file.txt</ns2:FachverfahrenObjektID>
+        <ns2:Schriftgutobjekt>
+            <ns2:Akte>
+                <ns2:Identifikation>
+                    <ns2:ID>e6c47537-0594-4d8c-be53-89b5e84a85f6</ns2:ID>
+                </ns2:Identifikation>
+                <ns2:AllgemeineMetadaten>
+                    <ns2:Betreff>${FIRMA}</ns2:Betreff>
+                    <ns2:Kennzeichen>Kenzeichunung</ns2:Kennzeichen>
+                    <ns2:Federfuehrung>Federfuehrung</ns2:Federfuehrung>
+                    <ns2:Aktenfuehrung>${FIRMA}</ns2:Aktenfuehrung>
+                    <ns2:Bemerkung>${FIRMA}</ns2:Bemerkung>
+                    <ns2:Medium>
+                        <code>001</code>
+                    </ns2:Medium>
+                    <ns2:Aktenplaneinheit>
+                        <ns2:Kennzeichen>10.01.01</ns2:Kennzeichen>
+                        <ns2:Inhaltsangabe>10.01.01</ns2:Inhaltsangabe>
+                        <ns2:BetreffKurz>10.01.01</ns2:BetreffKurz>
+                    </ns2:Aktenplaneinheit>
+                </ns2:AllgemeineMetadaten>
+                <ns2:Akteninhalt>
+                    <ns2:Vorgang>
+                        <ns2:Identifikation>
+                            <ns2:ID>e6c47537-0594-4d8c-be53-89b5e84a85f6</ns2:ID>
+                        </ns2:Identifikation>
+                        <ns2:AllgemeineMetadaten>
+                            <ns2:Betreff>2025 A und A Ausbildung und Arbeit Plus GmbH</ns2:Betreff>
+                            <ns2:Medium>
+                                <code>001</code>
+                            </ns2:Medium>
+                        </ns2:AllgemeineMetadaten>
+                        <ns2:Aktenbetreff>E</ns2:Aktenbetreff>
+                        <ns2:Dokument>
+                            <ns2:Identifikation>
+                                <ns2:ID>e6c47537-0594-4d8c-be53-89b5e84a85f6</ns2:ID>
+                            </ns2:Identifikation>
+                            <ns2:AllgemeineMetadaten>
+                                <ns2:Betreff>Dokument</ns2:Betreff>
+                                <ns2:Medium>
+                                    <code>001</code>
+                                </ns2:Medium>
+                            </ns2:AllgemeineMetadaten>
+
+                            <ns2:Version>
+                                <ns2:Nummer>1.0</ns2:Nummer>
+                                <ns2:Format>
+                                    <ns2:Name listURI="urn:xoev-de:xdomea:codeliste:dateiformat"
+                                        listVersionID="1.0">
+                                        <code>044</code>
+                                    </ns2:Name>
+                                    <ns2:Version>1.b</ns2:Version>
+                                    <ns2:Primaerdokument>
+                                        <ns2:Dateiname>00005000-0500-0050-0500-000050000000_file1.pdf</ns2:Dateiname>
+                                    </ns2:Primaerdokument>
+                                </ns2:Format>
+                            </ns2:Version>
+
+                            <ns2:Anlage>
+                                <ns2:Identifikation>
+                                    <ns2:ID>e6c47537-0594-4d8c-be53-89b5e84a85f6</ns2:ID>
+                                </ns2:Identifikation>
+                                <ns2:Version>
+                                    <ns2:Nummer>1.0</ns2:Nummer>
+                                    <ns2:Format>
+                                        <ns2:Name listURI="urn:xoev-de:xdomea:codeliste:dateiformat"
+                                            listVersionID="1.0">
+                                            <code>045</code>
+                                        </ns2:Name>
+                                        <ns2:Version>1.0</ns2:Version>
+                                        <ns2:Primaerdokument>
+                                            <ns2:Dateiname>10000000-0000-0000-0000-000000000000_file.txt</ns2:Dateiname>
+                                        </ns2:Primaerdokument>
+                                    </ns2:Format>
+                                </ns2:Version>
+                            </ns2:Anlage>
+
+                            <ns2:Anlage>
+                                <ns2:Identifikation>
+                                    <ns2:ID>e6c47537-0594-4d8c-be53-89b5e84a85f6</ns2:ID>
+                                </ns2:Identifikation>
+                                <ns2:Version>
+                                    <ns2:Nummer>1.0</ns2:Nummer>
+                                    <ns2:Format>
+                                        <ns2:Name listURI="urn:xoev-de:xdomea:codeliste:dateiformat"
+                                            listVersionID="1.0">
+                                            <code>100</code>
+                                        </ns2:Name>
+                                        <ns2:SonstigerName>md</ns2:SonstigerName>
+                                        <ns2:Version>1.0</ns2:Version>
+                                        <ns2:Primaerdokument>
+                                            <ns2:Dateiname>10000000-0000-0000-0000-000000000000_file.txt</ns2:Dateiname>
+                                        </ns2:Primaerdokument>
+                                    </ns2:Format>
+                                </ns2:Version>
+                            </ns2:Anlage>
+                        </ns2:Dokument>
+                    </ns2:Vorgang>
+                </ns2:Akteninhalt>
+            </ns2:Akte>
+        </ns2:Schriftgutobjekt>
+        <ns2:Ablageort>VIS API Test</ns2:Ablageort>
+    </ns2:SchriftgutobjektZumAblegen>
+</ns2:FVDaten.SGOAblegen.0605>
+""";
 
 var listTemplate = @"
 <Attchment>
@@ -31,9 +153,10 @@ KeyValuePair<string, string>[][]  listvars =
 
 var xdomeavars = new[]
 {
-    KeyValuePair.Create("IP_ADDRESS", "192.168.2.1"),
-    KeyValuePair.Create("NAME", "John DOE"),
+    KeyValuePair.Create("FIRMA", "Nice FIRMA GmbH"),
     KeyValuePair.Create("ATTACHMENTS", XdomeaPopulator.PopulateList(listvars,listTemplate))
 };
 
-Console.WriteLine(XdomeaPopulator.Populate(xdomeavars,template));
+var populated = XdomeaPopulator.Populate(xdomeavars,template);
+Console.WriteLine(populated);
+File.WriteAllText("output.xml", populated);
---
+++ README.md
@@ -0,0 +1,3 @@
+```shell
+ dotnet run; cp .\output.xml ./script; mv .\script\output.xml ./script/10000000-0000-0000-0000-000000000000_FVDaten.SGOAblegen.0605.xml;cd script;python ./make_request.py;rm 10000000-0000-0000-0000-000000000000_FVDaten.SGOAblegen.0605.xml; cd ../
+ ```
