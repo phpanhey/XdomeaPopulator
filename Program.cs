bb456491a0c5f777413ab2c9d8b89b6108bba700
Philipp Panhey <philipp.panhey@dataport.de>
2025-12-03 11:17:45 +0100

    Add XSD schemas and validation

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using Populator;
+﻿using System.Xml;
+using System.Xml.Schema;
+using Populator;
 
 // load template
 string template = File.ReadAllText(Path.Combine(AppContext.BaseDirectory, "template.xml"));
@@ -33,5 +35,55 @@ var xdomeavars = new[]
 };
 
 var populated = XdomeaPopulator.Populate(xdomeavars, template);
-Console.WriteLine(populated);
+
+string xsdPath = Path.Combine(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "xsd"), "xdomea.xsd");
+
+var isValid = IsValid(populated, xsdPath);
+
+Console.WriteLine($"XML is valid: {isValid}");
 File.WriteAllText("output.xml", populated);
+
+
+
+
+/**
+ * Validates an XML string against an XSD schema located at xsdPath.
+ **/
+
+bool IsValid(string xml, string xsdPath)
+{
+    bool isValid = true;    
+    var schemaSet = new XmlSchemaSet
+    {
+        XmlResolver = new XmlUrlResolver() 
+    };    
+    var xsdSettings = new XmlReaderSettings
+    {
+        XmlResolver = new XmlUrlResolver()
+    };
+
+    using (var xsdReader = XmlReader.Create(xsdPath, xsdSettings))
+    {
+        schemaSet.Add(null, xsdReader); 
+    }
+    
+    schemaSet.Compile();
+
+    var settings = new XmlReaderSettings
+    {
+        Schemas = schemaSet,
+        ValidationType = ValidationType.Schema
+    };
+
+    settings.ValidationFlags |= XmlSchemaValidationFlags.ReportValidationWarnings;
+
+    settings.ValidationEventHandler += (sender, e) =>
+    {
+        isValid = false;
+        Console.WriteLine($"{e.Severity}: {e.Message}");
+    };    
+    using var stringReader = new StringReader(xml);
+    using var xmlReader = XmlReader.Create(stringReader, settings);
+    while (xmlReader.Read()) { }
+    return isValid;
+}
